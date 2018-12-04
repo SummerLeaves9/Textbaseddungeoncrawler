dc5a101d9d990a7b8a663258547be37c8ad197f2
doome <anewma9@gmail.com>
2018-12-03 20:18:29 -0600

    Initial commit

---
+++ app/src/main/java/Dungeon.java
@@ -0,0 +1,7 @@
+public class Dungeon extends Object {
+   public static void main (String[] args) {
+       Player ralsei = new Player(5, 5, 5, 5, 5, 5, "Ralsei", "scarf whip");
+       System.out.println(ralsei.hp);
+
+   }
+}
