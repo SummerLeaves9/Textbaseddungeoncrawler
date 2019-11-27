75493753678eec7244a22ca127d4f1d0bb19cd65
doome <anewma9@gmail.com>
2019-11-27 13:13:22 -0600

    Spell class start

--- app/src/main/java/com/example/doome/text_baseddungeoncrawler/Spell.java
+++ app/src/main/java/com/example/doome/text_baseddungeoncrawler/Spell.java
@@ -5,20 +5,46 @@ public class Spell {
     public static byte spellIndex;
     public static String spellName;
 
-    public void basicHeal() {
+    public void basicHeal(Character c) {
         spellName = "Basic Heal";
+        byte tobeHealed = c.hp;
+        tobeHealed *= (byte) .35;
     }
 
     public void strongHeal() {
-        spellName = "StrongHeal";
+        spellName = "Strong Heal";
     }
 
     public void ultimateBlock() {
-
+        spellName = "Ultimate Block";
     }
 
     public void sphericon() {
-        
+        spellName = "Sphericon";
+    }
+
+    public void sphericonCharge() {
+        spellName = "Sphericon Charge";
+    }
+
+    public void sphericonSuperCharge() {
+        spellName = "Sphericon Supercharge";
+    }
+
+    public void luckyMarksman() {
+        spellName = "Lucky Marksman";
+    }
+
+    public void blindingLight() {
+        spellName = "Blinding Light";
+    }
+
+    public void fireball() {
+        spellName = "Fireball";
+    }
+
+    public void poison() {
+        spellName = "Poison";
     }
 
 }
